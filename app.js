afb9bc9291cf068311cc7459ac3aba1f4648b7e6
juanPabloGomezb <juanp-gomezb@unilibre.edu.co>
2024-08-31 09:51:58 -0500

    Foro

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ const data = {
       parent: 0,
       id: 1,
       content:
-        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      createdAt: "1 month ago",
+        "Los invitamos a nuestro comité de preparación de eventos del siguiente año, en este podremos abordar temas como la cantidad de eventos, las temáticas y el aporte que deberá tener cada uno.",
+      createdAt: "Hace un mes",
       score: 12,
       user: {
         image: {
           png: "./images/avatars/image-amyrobson.png",
           webp: "./images/avatars/image-amyrobson.webp",
         },
-        username: "amyrobson",
+        username: "Administradora",
       },
       replies: [],
     },
@@ -27,41 +27,41 @@ const data = {
       parent: 0,
       id: 2,
       content:
-        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-      createdAt: "2 weeks ago",
+        "Me gusta la idea, siempre he sido fan de los eventos, y me gusta compartir, podemos empezar este año con san valentín",
+      createdAt: "Hace dos semanas",
       score: 5,
       user: {
         image: {
           png: "./images/avatars/image-maxblagun.png",
           webp: "./images/avatars/image-maxblagun.webp",
         },
-        username: "maxblagun",
+        username: "mariaGomez",
       },
       replies: [
         {
           parent: 2,
           id: 1,
           content:
-            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-          createdAt: "1 week ago",
+            "Usted si ¿entiende?, según lo que entendí dijeron que para el otro año, no para septiembre de este año ",
+          createdAt: "Hace 1 semana",
           score: 4,
-          replyingTo: "maxblagun",
+          replyingTo: "mariaGomez",
           user: {
             image: {
               png: "./images/avatars/image-ramsesmiron.png",
               webp: "./images/avatars/image-ramsesmiron.webp",
             },
-            username: "ramsesmiron",
+            username: "jualianFelipe",
           },
         },
         {
           parent: 2,
           id: 1,
           content:
-            "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-          createdAt: "2 days ago",
+            "Creo que se podría expresar de una mejor forma, recuerde que estamos en un foro público y que el respeto es lo más importante.",
+          createdAt: "Hace dos días",
           score: 2,
-          replyingTo: "ramsesmiron",
+          replyingTo: "jualianFelipe",
           user: {
             image: {
               png: "./images/avatars/image-juliusomo.png",
