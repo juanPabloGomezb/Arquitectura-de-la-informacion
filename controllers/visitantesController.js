fb84128f5e9043683b1caf9592cfc3a5b5967ad8
juanPabloGomezb <juanp-gomezb@unilibre.edu.co>
2024-09-26 01:12:11 -0500

    controlador tablas

--- controllers/visitantesController.js
+++ controllers/visitantesController.js
@@ -12,6 +12,24 @@ exports.getVisitantes = (req, res) => {
     });
 };
 
+// Controlador para registrar un vehículo
+exports.registerVehiculo = (req, res) => {
+    const { matricula, modelo, visitante, nombre, apellido, numvivienda, telefono } = req.body;
+
+    if (!matricula || !modelo || !nombre || !apellido || !numvivienda || !telefono) {
+        return res.status(400).json({ message: 'Todos los campos son obligatorios.' });
+    }
+
+    const sql = 'INSERT INTO parqueadero (matricula, modelo, visitante, nombre, apellido, numvivienda, telefono) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    db.query(sql, [matricula, modelo, visitante, nombre, apellido, numvivienda, telefono], (error, results) => {
+        if (error) {
+            return res.status(500).json({ message: 'Error al registrar el vehículo.' });
+        }
+        res.status(201).json({ message: 'Vehículo registrado correctamente.' });
+    });
+};
+
+
 // Controlador para obtener las 5 notificaciones más recientes
 exports.getNotifications = (req, res) => {
     const query = `SELECT id, type, message, details, created_at
